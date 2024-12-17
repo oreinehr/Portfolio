7d74dfab094f59bd648f2fcf84785e4ffae19e88
oreinehr <guireinehr02@gmail.com>
2024-12-17 13:29:39 -0300

    Ajustes Finais

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['sjc.microlink.io', 'api.microlink.io'], // Adicionando os domínios permitidos
+      domains: ['sjc.microlink.io', 'api.microlink.io', 'vercel.app', 'github.com'],
     },
   };
   
