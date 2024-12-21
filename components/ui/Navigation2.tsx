c4cc57c4bf9e1750310135579e07b1a7d813ac75
oreinehr <guireinehr02@gmail.com>
2024-12-21 12:31:12 -0300

    header animation

--- components/ui/Navigation2.tsx
+++ components/ui/Navigation2.tsx
@@ -1,14 +1,45 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-    
 
 export default function Navigation() {
+  // Estado para controle da visibilidade do navbar
+  const [visible, setVisible] = useState(true)
+  let lastScrollY = 0
+
+  // Função para lidar com o evento de rolagem
+  const handleScroll = () => {
+    if (typeof window !== 'undefined') {
+      // Verifica a direção do scroll
+      if (window.scrollY > lastScrollY) {
+        // Rolando para baixo, esconder o navbar
+        setVisible(false)
+      } else {
+        // Rolando para cima, mostrar o navbar
+        setVisible(true)
+      }
+      // Atualiza a última posição da rolagem
+      lastScrollY = window.scrollY
+    }
+  }
+
+  // Usar useEffect para adicionar o ouvinte de evento de rolagem
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    // Limpeza do evento ao desmontar o componente
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <motion.nav 
+    <motion.nav
       initial={{ opacity: 0, y: -20 }}
-      animate={{ opacity: 20, y: 0 }}
+      animate={{ opacity: visible ? 1 : 0, y: visible ? 0 : -60 }} // Controlando a visibilidade
+      transition={{ duration: 0.3 }}
       className="fixed w-full top-0 z-50 bg-transparent"
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,12 +48,12 @@ export default function Navigation() {
             <Link href="/" className="text-white hover:text-gray-600 transition-colors">
               Home
             </Link>
-            
+
             <Link href="/templates" className="text-white hover:text-gray-600 transition-colors">
               Projetos
             </Link>
           </div>
-          
+
           <Link href="/" className="flex-shrink-0">
             <span className="sr-only">Your Logo</span>
             <motion.div
@@ -32,11 +63,8 @@ export default function Navigation() {
               GR
             </motion.div>
           </Link>
-          
-    
         </div>
       </div>
     </motion.nav>
   )
 }
-
